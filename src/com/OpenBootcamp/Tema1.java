fe76057d43d6323f4177a13c6913370d2a718b11
CAR0NTE77 <122113571+CAR0NTE77@users.noreply.github.com>
2023-02-08 01:35:18 -0400

    Initial commit

---
+++ src/com/OpenBootcamp/Tema1.java
@@ -0,0 +1,37 @@
+package com.OpenBootcamp;
+
+public class Tema1 {
+    public static void main(String[] args) {
+        //enteros
+        byte variable1 = 3;
+        short variable2 = 30;
+        int variable3 = 300;
+        long variable4 = 3000;
+        //imprimir numeros enteros
+        System.out.println("ENTEROS");
+        System.out.println("la variable byte es: " + variable1);
+        System.out.println("la variable short es: " + variable2);
+        System.out.println("la variable int es: " + variable3);
+        System.out.println("la variable long es: " + variable4);
+        //decimales
+        float variable5 = 3.3f;
+        double variable6 = 333.33;
+        //imprimir decimales
+        System.out.println("DECIMALES");
+        System.out.println("la variable float es: " + variable5);
+        System.out.println("la variable double es: " + variable6);
+        //booleano
+        boolean variable7 = true;
+        boolean variable8 = false;
+        //imprimir boolean
+        System.out.println("BOOLEAN");
+        System.out.println("la variable7 booleana es: " + variable7 );
+        System.out.println("la variable8 booleana es: " + variable8 );
+        //TEXTO
+        char variable9 = 'a';
+        String variable10 = "Este es un texto String";
+        System.out.println("TEXTO");
+        System.out.println("la variable char es: " + variable9);
+        System.out.println("la variable String es: " + variable10);
+    }
+}
